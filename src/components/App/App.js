71885ee6d9e0a9cc821dc1cc8f06886018f9330a
sr-hub <spencer.r1@outlook.com>
2020-01-27 13:51:03 -0500

    Added Api Auth.js, Made App Hooks
    Added an auth.js file in api folder in components with user auth
      functions
    Refactored old code in App.js to match hooks syntax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, withRouter } from 'react-router-dom'
 
 import Jokes from '../components/routes/Jokes'
@@ -14,37 +14,75 @@ import SignIn from '../SignIn/SignIn'
 import SignOut from '../SignOut/SignOut'
 import ChangePassword from '../ChangePassword/ChangePassword'
 
-const App = props => (
-  <React.Fragment>
-    <main className="container">
-      <h3>{props.location.state ? props.location.state.msg : null}</h3>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/' render={() => (
-        <Jokes/>
-      )} />
-      <Route exact path='/jokes/:id' render={({ match }) => (
-        <Joke user={user} match={match}/>
-      )} />
-      <AuthenticatedRoute user={user} path='/create-joke' render={() => (
-        <JokeCreate alert={this.alert} user={user} />
-      )} />
-      <AuthenticatedRoute user={user} path='/jokes/:id/edit' render={({ match }) => (
-        <JokeEdit match={match} alert={this.alert} user={user} />
-      )} />
-      <Route path='/sign-up' render={() => (
-        <SignUp alert={this.alert} setUser={this.setUser} />
-      )} />
-      <Route path='/sign-in' render={() => (
-        <SignIn alert={this.alert} setUser={this.setUser} />
-      )} />
-      <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-        <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
-      )} />
-      <AuthenticatedRoute user={user} path='/change-password' render={() => (
-        <ChangePassword alert={this.alert} user={user} />
-      )} />
-    </main>
-  </React.Fragment>
-)
+// class App extends Component {
+//   constructor () {
+//     super()
+//
+//     state = {
+//       user: null,
+//       alerts: []
+//     }
+//   }
+//
+//   setUser = user => setState({ user })
+//
+//   clearUser = () => setState({ user: null })
+//
+//   alert = ({ heading, message, variant }) => {
+//     setState({ alerts: [...state.alerts, { heading, message, variant }] })
+//   }
+
+const App = props => {
+  const [user, setUser] = useState(null)
+  const [alert, setAlert] = useState([])
+
+  const clearUser = () => setUser(null)
+
+  const alerts = ({ heading, message, variant }) => {
+    setAlert({ alerts: [...alerts, { heading, message, variant }] })
+  }
+
+  return (
+    <React.Fragment>
+      <Header user={user} />
+      {alerts.map((alert, index) => (
+        <AutoDismissAlert
+          key={index}
+          heading={alert.heading}
+          variant={alert.variant}
+          message={alert.message}
+        />
+      ))}
+      <main className="container">
+        <h3>{props.location.state ? props.location.state.msg : null}</h3>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/' render={() => (
+          <Jokes/>
+        )} />
+        <Route exact path='/jokes/:id' render={({ match }) => (
+          <Joke user={user} match={match}/>
+        )} />
+        <AuthenticatedRoute user={user} path='/create-joke' render={() => (
+          <JokeCreate alert={alert} user={user} />
+        )} />
+        <AuthenticatedRoute user={user} path='/jokes/:id/edit' render={({ match }) => (
+          <JokeEdit match={match} alert={alert} user={user} />
+        )} />
+        <Route path='/sign-up' render={() => (
+          <SignUp alert={alert} setUser={setUser} />
+        )} />
+        <Route path='/sign-in' render={() => (
+          <SignIn alert={alert} setUser={setUser} />
+        )} />
+        <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+          <SignOut alert={alert} clearUser={clearUser} user={user} />
+        )} />
+        <AuthenticatedRoute user={user} path='/change-password' render={() => (
+          <ChangePassword alert={alert} user={user} />
+        )} />
+      </main>
+    </React.Fragment>
+  )
+}
 
 export default withRouter(App)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App'
+import App from './components/App/App'
 import { HashRouter } from 'react-router-dom'
 
 const appJsx = (
